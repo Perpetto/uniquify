944acc0c89709bcb95b94e666508949a6d1b9864
Vladimir Alekiev <vlad@edhub.io>
2014-12-15 13:21:50 +0200

    Adding finder for uniquified objects. All of these are working now: - Klass.find(1) - Klass.find('1') - Klass.find('some_long_token_value')
    I ripped the custom finder from mongoid_token gem:
    https://github.com/thetron/mongoid_token

--- lib/uniquify.rb
+++ lib/uniquify.rb
@@ -1,3 +1,4 @@
+p 'uniquifff'
 module Uniquify
   def self.included(base)
     base.extend ClassMethods
@@ -24,7 +25,24 @@ module Uniquify
             end
           end
         end
+        define_custom_token_finder_for(self, name)
       end
+    end
+
+
+    # ripped from mongoid_token gem
+    def define_custom_token_finder_for(klass, field_name = :token)
+      klass.define_singleton_method(:"find_by_#{field_name.to_s}") do |token|
+        self.find_by(field_name.to_sym => token)
+      end
+
+      klass.define_singleton_method :"find_with_#{field_name}" do |*args| # this is going to be painful if tokens happen to look like legal object ids
+        args.all?{|arg| arg.to_i != 0 } ? send(:"find_without_#{field_name}",*args) : klass.send(:"find_by_#{field_name.to_s}", args.first)
+      end
+
+      # this craziness taken from, and then compacted into a string class_eval
+      # http://geoffgarside.co.uk/2007/02/19/activesupport-alias-method-chain-modules-and-class-methods/
+      klass.class_eval("class << self; alias_method_chain :find, :#{field_name} if self.method_defined?(:find); end")
     end
 
   end
